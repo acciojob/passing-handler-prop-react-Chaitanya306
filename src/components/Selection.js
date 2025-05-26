3a77a635595b33bf0ed8a8b576e1dc58b4c5a1ef
Chaitanya306 <chaitanya09306@gmail.com>
2025-05-26 12:50:56 +0530

    Solution

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,18 @@
+import React, { useState } from 'react'
+
+function Selection(props) {
+  const { nextBackground,applyColor } = props;  
+  const [backgroundColor, setBackgroundColor] = useState(nextBackground);
+  
+  //console.log(nextBackground)  
+  return (
+    <div className='fix-box subheading' style={{backgroundColor: `${backgroundColor.background}` }}
+      onClick={() => { applyColor(setBackgroundColor)}} >
+      Selection
+    </div>
+  )
+}
+
+export default Selection
+
 
